40acb68371fde7645e07ded399b2c8261e0013f0
CassiaHelenSrt <cassiahelen9213@gmail.com>
2026-02-12 11:59:54 -0300

    layout login responsivo, visibility password

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,13 +1,29 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: 'input.component.html',
   styleUrls: ['input.component.scss'], // ← CORRETO
 })
 export class InputComponent {
   @Input() type: string = 'text';
   @Input() placeholder: string = '';
+  @Input() icon: string = '';
+
+  hidePassword: boolean = true;
+
+  togglePassword() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  get inputType() {
+    if (this.type === 'password') {
+      return this.hidePassword ? 'password' : 'text';
+    }
+
+    return this.type;
+  }
 }
