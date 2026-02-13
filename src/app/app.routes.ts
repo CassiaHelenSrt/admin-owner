48bdd8e148b538bbfc6e490aa95c0baf48e53073
CassiaHelenSrt <cassiahelen9213@gmail.com>
2026-02-13 11:29:06 -0300

    layout sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,28 @@ export const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full',
   },
+
+  // Login (fora do layout)
   {
     path: 'login',
     loadComponent: () =>
       import('./features/auth/pages/login/login.component').then((m) => m.LoginComponent),
   },
-  //   {
-  //     path: 'dashboard',
-  //     canActivate: [authGuard],
-  //     loadComponent: () =>
-  //       import('./features/dashboard/pages/dashboard/dashboard.component').then(
-  //         (m) => m.DashboardComponent,
-  //       ),
-  //   },
+
+  // Rotas que usam o layout com sidebar
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/main-layout/main-layout.component').then((m) => m.MainLayoutComponent),
+
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/admin/dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent,
+          ),
+      },
+    ],
+  },
 ];
---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+// import { SidebarComponent } from '@shared/sidebar/sidebar.component';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent],
+
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss'],
+})
+export class MainLayoutComponent {}
