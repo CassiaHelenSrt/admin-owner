c381f74d9c70807b78dccd8e6e00c3069b8165f4
CassiaHelenSrt <cassiahelen9213@gmail.com>
2026-03-09 15:01:38 -0300

    layout-scheduling-dinamico-ajuste

--- src/app/features/admin/pages/scheduling/scheduling.component.ts
+++ src/app/features/admin/pages/scheduling/scheduling.component.ts
@@ -75,29 +75,49 @@ export class SchedulingComponent {
       status: 'canceled',
       date: '2026-03-07',
     },
+    {
+      client: 'Juliana Almeida',
+      service: 'Volume Brasileiro',
+      start: '19:00',
+      end: '20:00',
+      status: 'canceled',
+      date: '2026-03-08',
+    },
+    {
+      client: 'Juliana Almeida',
+      service: 'Volume Brasileiro',
+      start: '08:00',
+      end: '09:00',
+      status: 'confirmed',
+      date: '2026-03-09',
+    },
   ];
 
+  appointmentsOfDay: Appointment[] = [];
   appointmentMap: { [hour: string]: Appointment } = {};
 
   allHours: string[] = [];
+  formattedDate = '';
 
   ngOnInit() {
     this.allHours = [...this.morningHours, ...this.afternoonHours, ...this.nightHours];
+    this.updateDate();
     this.generateMap();
+    this.getAppointmentsOfDay();
   }
 
-  getFormattedDate() {
+  updateDate() {
     const d = this.currentDate;
 
     const year = d.getFullYear();
     const month = String(d.getMonth() + 1).padStart(2, '0');
     const day = String(d.getDate()).padStart(2, '0');
 
-    return `${year}-${month}-${day}`;
+    this.formattedDate = `${year}-${month}-${day}`;
   }
 
   generateMap() {
-    const date = this.getFormattedDate();
+    const date = this.formattedDate;
 
     this.appointmentMap = {};
 
@@ -109,24 +129,31 @@ export class SchedulingComponent {
   }
 
   getAppointment(hour: string) {
-    const date = this.getFormattedDate();
+    const date = this.formattedDate;
+    console.log(date);
 
     return this.appointments.find((a) => a.date === date && a.start === hour);
   }
 
   getAppointmentsOfDay() {
-    const date = this.getFormattedDate();
-
-    return this.appointments.filter((a) => a.date === date);
+    this.appointmentsOfDay = this.appointments.filter((a) => a.date === this.formattedDate);
   }
 
   nextDay() {
     this.currentDate.setDate(this.currentDate.getDate() + 1);
     this.currentDate = new Date(this.currentDate);
+
+    this.updateDate();
+    this.generateMap();
+    this.getAppointmentsOfDay();
   }
 
   prevDay() {
     this.currentDate.setDate(this.currentDate.getDate() - 1);
     this.currentDate = new Date(this.currentDate);
+
+    this.updateDate();
+    this.generateMap();
+    this.getAppointmentsOfDay();
   }
 }
